cbb0e3c0754a12c0dc82a5ae8850281dc610f3b7
Ranga <rengan76@users.noreply.github.com>
2018-10-17 10:48:59 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+
+define('HOST', 'efi-api-db.cziappduivss.us-east-1.rds.amazonaws.com'); ///DEV
+///
+// define('HOST','instantrebate-prod-v00.civhiclqybq8.us-east-1.rds.amazonaws.com'); //PROD
+define('DBNAME','raas'); // Production
+// define('DBNAME','raas'); //Development
+define('UNAME', 'admin');
+//define('UNAME', 'dbadmin'); //PROD
+// define('PASSWORD','k9ozYBB7eBvhYp8PqHXr'); //PROD
+define('PASSWORD','3fiusers');
+
+define('TABLE_NAME','customers_masssave_20180707');
+
+define('SMARTY_STREET_URL', 'https://us-street.api.smartystreets.com/street-address?auth-id=b031f5a4-b305-9af2-c400-76aa7caef41b&auth-token=29r7456yf57OGIuK9z7F&');
+
+ //DEV
+ //define('CUSTOMERVALIDATE_URL', 'https://s1ya09uxof.execute-api.us-east-1.amazonaws.com/CustomerValidator_1/validate/byaddresslastname');
+
+ //PROD
+//define('CUSTOMERVALIDATE_URL', 'https://f53bncyuxb.execute-api.us-east-1.amazonaws.com/CustomerValidator-2018-06-28/validate/byaddresslastname');
+
